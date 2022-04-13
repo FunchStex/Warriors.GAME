c467277f781a6d33acaf567cdd89518f986a1026
Signorlupo22 <88050327+Signorlupo22@users.noreply.github.com>
2022-04-13 11:59:41 +0200

    Add files via upload

--- webserver/webserver/Pachetto.cs
+++ webserver/webserver/Pachetto.cs
@@ -8,16 +8,15 @@ namespace BasicWeb
 {
     internal class Pachetto
     {
-        public float[] posizione { get; set; }
-        public float[] velocity { get; set; }
-        public int id { get; set; }
+        public float[] posizione { get; set; } = {0,0};
+        public float[] velocity { get; set; } = { 0, 0 };
+        public int id { get; set; } = 200;
         public int vivo { get; set; } = 2;
-        public int persone {  get; set; }
+        public int persone { get; set; } = 0;
         public bool apple { get; set; } = false;
-        public float applex { get; set; }
-        public float appley { get; set; }
         public bool Colpito { get; set; } = true;
-        public float Time { get; set; } = 5;
+        public float Time { get; set; } = 10;
+        public bool AppleVivo { get; set; } = false;
 
 
     }
--- webserver/webserver/Program.cs
+++ webserver/webserver/Program.cs
@@ -28,9 +28,22 @@ namespace BasicWeb
 
         public static bool Spawn=true;
         public static float Timer = 10;
-        public static float[] Postionx= new float[100];
-        public static float[] Postiony = new float[100];
         public static int MeleCount=0;
+        public static string html =
+            "<!DOCTYPE>" +
+            "<html>" +
+            "  <head>" +
+            "    <title>HttpListener Example</title>" +
+            "  </head>" +
+            "  <body>" +
+            "    <p>Page Views: {0}</p>" +
+            "    <form method=\"post\" action=\"shutdown\">" +
+            "      <input type=\"submit\" value=\"Shutdown\" {1}>" +
+            "    </form>" +
+            "  </body>" +
+            "</html>";
+        public static float[,] _dis=new float[13,13];
+
 
 
 
@@ -49,97 +62,125 @@ namespace BasicWeb
         public static void SpawnApple()
         {
 
-            if (dataset.Count>0&& MeleCount <= 5 && Spawn)
+            if (MeleCount < 3 && Spawn)
             {
                 Random random = new Random();
+                Pachetto Mela = new Pachetto();
+                string count = (100 + MeleCount).ToString();
 
-                dataset["1"].applex = (float)random.NextDouble() * (float)5.5;
-                dataset["1"].applex -= (float)2.8;
-                dataset["1"].appley = (float)random.NextDouble() * (float)2.35;
-                dataset["1"].appley -= (float)1.25;
-                dataset["1"].apple = true;
-                Postionx[MeleCount] = dataset["1"].applex;
-                Postiony[MeleCount] = dataset["1"].appley;
+                float x = (float)random.NextDouble() * (float)5.4;
+                x -= (float)2.8;
+                float y = (float)random.NextDouble() * (float)2.25;
+                y-= (float)1.25;
+
+
+                Mela.posizione[0] = x;
+                Mela.posizione[1] = y;
+                Mela.AppleVivo = true;
+                bool keyExists = dataset.ContainsKey(count.ToString());
+                if (!keyExists)
+                    dataset.Add(count,Mela);
+
+                dataset[count.ToString()] = Mela;
+
                 MeleCount++;
-                Console.WriteLine("pozione cambiata x{0},y{1},Mele: {2}", dataset["1"].appley, dataset["1"].applex, MeleCount);
+                Console.WriteLine("pozione cambiata x{0},y{1},Mele: {2}", dataset[count].posizione[0], dataset[count].posizione[1], MeleCount);
                 Spawn = false;
 
             }
             else if (dataset.Count>0&&!Spawn)
             {
                 dataset["1"].apple = false;
-                Console.WriteLine(dataset["1"].apple);
                 Spawn = true;
+            }else
+            {
+                MeleCount = 0;
             }
         }
         public static async Task CheckDeath()
         {
-
+             int i = 0;
             foreach (KeyValuePair<string, Pachetto> kvp in dataset)
             {
                 float x = kvp.Value.posizione[0];
                 float y = kvp.Value.posizione[1];
                 float vx = kvp.Value.velocity[0];
                 float vy = kvp.Value.velocity[1];
-                if (Postiony != null)
-                {
-                    for(int i=0; i<Postiony.Length; i++)
+
+
+                 int j = 0;
+                foreach (KeyValuePair<string, Pachetto> kvp2 in dataset)
+                 {
+                    if (kvp.Value.id!=200&&kvp2.Value.id!=200)
                     {
-                        float disMela = (float)Math.Sqrt(Math.Pow(x - Postionx[i], 2) + Math.Pow(y - Postiony[i], 2));
+                        if (kvp.Key != kvp2.Key)
+                        {
+                            float x2=kvp2.Value.posizione[0];
+                            float y2=kvp2.Value.posizione[1];
+                            float vx2=kvp2.Value.velocity[0];
+                            float vy2=kvp2.Value.velocity[1];
+
+
+                            float dis = (float)Math.Sqrt(Math.Pow(x - x2, 2) + Math.Pow(y - y2, 2));
+                            _dis[i,j] = dis;
+                             if (_dis[i,j] <= 0.08 && ( Math.Abs(vx) > Math.Abs(vx2) || Math.Abs(vy) > Math.Abs(vy2) ))
+                             {
+                                bool colpito=kvp2.Value.Colpito;
+                                if (colpito)                                
+                                {
+                                    kvp2.Value.Colpito = false;
+                                    kvp2.Value.vivo--;
+                                    Console.WriteLine(colpito);
+                                    Console.Write("in collisione");
+
+
+                                }
+                                Console.WriteLine(kvp.Value.vivo);
+                             }
+                            Console.WriteLine("id:{0},vita:{1}", kvp.Value.id, kvp.Value.vivo);
+                            //if (_dis[i,j] > 0.08)
+                            //{
+                            //    kvp2.Value.Colpito = true;
+                            //    Console.WriteLine(_dis[i,j]);
+
+                            //}
+
+                        }
+
+
+
+                    }
+                    else if(kvp.Value.id!=200)
+                    {
+                        float disMela = (float)Math.Sqrt(Math.Pow(x - kvp2.Value.posizione[0], 2) + Math.Pow(y - kvp2.Value.posizione[1], 2));
                         if (disMela < 0.1)
                         {
                             if (kvp.Value.vivo < 3)
                             {
                                 kvp.Value.vivo++;
                             }
-                        }
+                                kvp2.Value.AppleVivo = false;
+                                kvp2.Value.posizione[0] = 5;
+                                kvp2.Value.posizione[1] = 5;
+                                Console.WriteLine("viata umentata");
 
+                            
+                        }
                     }
 
-                }
-
-                foreach (KeyValuePair<string, Pachetto> kvp2 in dataset)
-                   {
-                    if (kvp.Key != kvp2.Key)
+                    if (kvp.Value.vivo <= 0&&(kvp.Value.id!=200))
                     {
-                        float x2=kvp2.Value.posizione[0];
-                        float y2=kvp2.Value.posizione[1];
-                        float vx2=kvp2.Value.velocity[0];
-                        float vy2=kvp2.Value.velocity[1];
-
-
-                        float dis;
-                        dis = (float)Math.Sqrt(Math.Pow(x - x2, 2) + Math.Pow(y - y2, 2));
-                            if (dis < 0.1 && ((Math.Abs(vx) > Math.Abs(vx2) || Math.Abs(vy) > Math.Abs(vy2)))&&kvp2.Value.Colpito)
-                            {
-                                Console.WriteLine("mortoooooooooooooooooooooooooooooooooooooooooooooooooooooo");
-                                kvp2.Value.vivo=kvp2.Value.vivo-1;
-                                kvp.Value.Colpito = false;
-                            }       
-                            //else if (dis < 0.1 && ((Math.Abs(vx) < Math.Abs(vx2) || Math.Abs(vy) < Math.Abs(vy2)))&&kvp2.Value.Colpito)
-                            //{
-                            //    kvp.Value.vivo=kvp.Value.vivo - 1;
-                            //    kvp2.Value.Colpito = false;
-                            //    Console.WriteLine("mortoooooooooooooooooooooooooooooooooooooooooooooooooooooo");
-
-                            //}
-
-                        if (!kvp.Value.Colpito)
-                        {
-                            kvp.Value.Time-=(float)0.01;
-                            if(kvp.Value.Time < 0)
-                            {
-                                kvp.Value.Colpito=true;
-                                kvp.Value.Time=5;
-
-                            }
-                        }
-
-
+                        kvp.Value.posizione[0] = 100 + kvp.Value.posizione[0];
+                        kvp.Value.posizione[1] = 100 + kvp.Value.posizione[1];
                     }
+                    j++;
 
                 }
+
+                    i++;
             }
+
+ 
         }
         public static async Task inattivita()
         {
@@ -147,13 +188,14 @@ namespace BasicWeb
             {
                 float x = kvp.Value.posizione[0];
                 float y = kvp.Value.posizione[1];
-                Thread.Sleep(20000);
+               Task.Delay(20000);
                 float newx=kvp.Value.posizione[0];
                 float newy = kvp.Value.posizione[1];
 
                 if(x == newx && y == newy)
                 {
-                    dataset.Remove(kvp.Key);
+                    kvp.Value.posizione[0] = 100+kvp.Value.posizione[0];
+                    kvp.Value.posizione[1] = 100+kvp.Value.posizione[1];
                 }
 
             }
@@ -175,14 +217,6 @@ namespace BasicWeb
             else Timer -= (float)0.1;
             CheckDeath();
 
-
-
-
-            foreach (KeyValuePair<string, Pachetto> kvp in dataset)
-            {
-                Console.WriteLine("Key: {0}, Value x: {1}, Value y:{2}, vita:{3},player online: {4},apple x{5},y{6},{7}", kvp.Key, kvp.Value.posizione[0], kvp.Value.posizione[1], kvp.Value.vivo, kvp.Value.persone, kvp.Value.applex, kvp.Value.appley, kvp.Value.apple);
-
-            }
         }
         public static async Task HandleIncomingConnections()
         {
@@ -222,7 +256,6 @@ namespace BasicWeb
                     {
                         if (player.id > 100)
                         {
-                            //Console.WriteLine(player.persone);
                             player.persone = pageViews;
                             pageViews++;
 
@@ -232,13 +265,17 @@ namespace BasicWeb
 
 
                         dataset[player.id.ToString()] = player;
-                        await Task.Run(() => {
+                        await Task.Run(() =>
+                        {
                             calcolo();
-                          });
-                        await Task.Run(() => {
                             //inattivita();
                         });
+                        bool keyExists = dataset.ContainsKey("1");
+                        if (keyExists)
+                        {
 
+                        }
+                            //Console.WriteLine("la vita di 1 eeeee {0},", dataset["1"].vivo);
                         string data = System.Text.Json.JsonSerializer.Serialize(dataset);
                         request.Content = new StringContent(data,Encoding.UTF8,"application/json");
 
@@ -249,14 +286,14 @@ namespace BasicWeb
                         byte[] buffer = System.Text.Encoding.UTF8.GetBytes(data);
                         resp.ContentType = "application/json";
                         resp.ContentEncoding = Encoding.UTF8;
-;
 
 
                         await resp.OutputStream.WriteAsync(buffer, 0, buffer.Length);
                     }
-                }
+ 
                     resp.Close();
 
+                }
             }
         }
 
